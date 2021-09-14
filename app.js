2433fb66e56dab6cc22a00da302a19feab070ac3
jeremia49 <jeremiamanurungganteng@gmail.com>
2021-09-14 14:41:04 +0700

    ➕ meta tag viewport in html

--- app.js
+++ app.js
@@ -82,6 +82,7 @@ app.get("/",(req, res)=>{
         <html>
             <head>
                 <title>MP4 to HLS</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
             </head>
             <body style="background-color:#4a4a4a; color:#ffffff" >
@@ -194,6 +195,7 @@ app.post('/create',
             <html>
                 <head>
                     <title>MP4 to HLS</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
                 </head>
                 <body style="background-color:#4a4a4a; color:#ffffff" >
@@ -218,6 +220,7 @@ app.post('/create',
             <html>
                 <head>
                     <title>MP4 to HLS</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
                 </head>
                 <body style="background-color:#4a4a4a; color:#ffffff" >
